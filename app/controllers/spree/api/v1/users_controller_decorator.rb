aebc5460ed7582d89c4c9117e0b98fb904bd608f
Eric Anderson <eric@pixelwareinc.com>
2017-05-01 20:20:35 -0400

    Fix Spree 3.0 compat for controller namespace
    I thought my constant handing would have worked for 3.0 but turns out
    that Spree::Api::V1::UsersController is already defined somehow. It
    actually returns a reference to Spree::Api::UsersController.
    
    I did a bit of research but couldn't find where this alias is actually
    defined. It appears in 2012 Spree had a `V1` namespace. Then it got
    unfashionable and was replaced with version_cake. Then it got
    fashionable again in 2015 and the reversed course.
    
    I'm guessing somewhere in there an alias compatibility was added but
    I'm not completely sure.
    
    Anyway, to satisfy the autoloading we take the found constant and define
    it as a real constant. At that point we have something that work on
    both Spree 3.0 and Spree >= 3.1.

--- app/controllers/spree/api/v1/users_controller_decorator.rb
+++ app/controllers/spree/api/v1/users_controller_decorator.rb
@@ -1,12 +1,7 @@
-users_controller = begin
-  # Spree >= 3.1
-  Spree::Api::V1::UsersController
-rescue NameError
-  # Spree ~> 3.0
-  Spree::Api::UsersController
-end
+users_controller = Spree::Api::V1::UsersController
+Spree::Api::V1.const_set :UsersController, Spree::Api::UsersController if users_controller.name == 'Spree::Api::UsersController'
 
-users_controller.class_eval do
+Spree::Api::V1::UsersController.class_eval do
   before_action :authenticate_user, :except => [:sign_up, :sign_in]
   prepend_before_action :allow_params_authentication!, only: :sign_in
 
